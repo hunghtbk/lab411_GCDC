51d01e1f63cde2a1a0639950366db4ac3986dd5c
hunght <hunght@hunght-bk.(none)>
2016-01-05 18:49:10 +0700

    lab

---
+++ GCDCCommStackBranch/src/kernel/ath/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_/mnt/hc/jan/SoftDev/TeamProjects/Spits/SubVersion/Spits/wp04/GCDC/GCDCCommStackBranch/src/kernel/ath/built-in.o :=  rm -f /mnt/hc/jan/SoftDev/TeamProjects/Spits/SubVersion/Spits/wp04/GCDC/GCDCCommStackBranch/src/kernel/ath/built-in.o; ar rcs /mnt/hc/jan/SoftDev/TeamProjects/Spits/SubVersion/Spits/wp04/GCDC/GCDCCommStackBranch/src/kernel/ath/built-in.o
